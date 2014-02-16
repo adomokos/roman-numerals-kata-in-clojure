9edfd2eebe97955106f1e9f89d605b137c2d5205
Attila Domokos <adomokos@gmail.com>
2014-02-16 14:54:05 -0600

    Expressing roman numeral conversions in a clearer way

--- src/roman_numerals/printer.clj
+++ src/roman_numerals/printer.clj
@@ -3,9 +3,14 @@
 
 (defn- find-highest-reducer [for-number]
   (cond
-    (>= for-number 49) {:arabic 50 :roman "L"}
-    (>= for-number 9) {:arabic 10 :roman "X"}
-    (>= for-number 4)  {:arabic 5  :roman "V"}
+    (>= for-number 100){:arabic 100 :roman "C"}
+    (>= for-number 90) {:arabic 90 :roman "XC"}
+    (>= for-number 50) {:arabic 50 :roman "L"}
+    (>= for-number 40) {:arabic 40 :roman "XL"}
+    (>= for-number 10)  {:arabic 10 :roman "X"}
+    (= for-number 9)  {:arabic 9 :roman "IX"}
+    (>= for-number 5)  {:arabic 5  :roman "V"}
+    (= for-number 4)   {:arabic 4  :roman "IV"}
     :else {:arabic 1 :roman "I"}))
 
 (defn convert
@@ -14,9 +19,7 @@
     (convert input ""))
   ([input converted]
     (let [highest-reducer (find-highest-reducer input)
-          decremented-product (str converted "I" (:roman highest-reducer))
           product (str converted (:roman highest-reducer))]
       (cond
-        (= 0 input) converted
-        (= input (dec (:arabic highest-reducer))) decremented-product
+        (= 0 input) converted ;; No need to convert more - exit
         :else (convert (- input (:arabic highest-reducer)) product)))))
